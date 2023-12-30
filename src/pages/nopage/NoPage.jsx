02ce3fe557bc3a6b9763b6f59f98e24530880ee6
nguyenduy <nguyenduy011201@gmail.com>
2023-12-31 00:27:47 +0700

    update nopage

--- src/pages/nopage/NoPage.jsx
+++ src/pages/nopage/NoPage.jsx
@@ -2,27 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./style.scss";
 
 export default function NoPage() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(10);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      if (count < 10) {
-        setCount(count + 1);
+      if (count > 0) {
+        setCount(count - 1);
       } else {
         clearInterval(timer);
         window.location = "/login";
       }
     }, 1000);
 
-    // Xóa hẹn giờ khi component bị unmounted
     return () => {
       clearInterval(timer);
     };
   }, [count]);
 
   return (
-    <div className="container-fluid m-0 p-0 wrap-home vh-100 bg-lazy">
-      Countdown: {count}
+    <div className="container-fluid m-0 p-0 wrap-home vh-100 bg-lazy d-flex justify-content-center align-items-center">
+      <div>
+        <h3 className="text-center text-danger">Trang này không tồn tại</h3>
+        <p className="mt-2">Hệ thống sẽ tự chuyển về trang đăng nhập sau {count}</p>
+      </div>
     </div>
   );
 }
